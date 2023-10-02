4e61b93b144f0b92162b38ba31666b5e2d780872
Moiz Ahmed <moizah96@gmail.com>
2023-10-01 22:45:45 -0400

    Loaded CSS and hero image to the configuration

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+import './style.css';
+import hero from './hero.jpeg'
+
+const content = document.querySelector("content")
+
+const heroImage = document.createElement("img")
+heroImage.src = hero;
+
+content.appendChild(heroImage)
+
 console.log('Hello World!')

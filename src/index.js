0c5563c90695b665ce3c9ded893e4490da94be03
Moiz Ahmed <moizah96@gmail.com>
2023-10-02 22:04:06 -0400

    Coded front page to Javascript to load when running the build

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import './style.css';
 import hero from './hero.jpeg'
 
-const content = document.querySelector("content")
+const content = document.getElementById("content")
 
+// Header 1
+let hOne = document.createElement("h1")
+hOne.textContent = "Webpack Restaurant"
+
+// Header 2
+let hTwo = document.createElement("h2")
+hTwo.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores laudantium dolorum voluptate, perspiciatis mollitia esse a sequi officiis laborum harum!"
+
+// Hero Image
 const heroImage = document.createElement("img")
 heroImage.src = hero;
 
+// Header 4
+let hFour = document.createElement("h4")
+hFour.textContent = "Lorem ipsum dolor sit amet."
+
+content.appendChild(hOne)
+content.appendChild(hTwo)
 content.appendChild(heroImage)
+content.appendChild(hFour)
 
 console.log('Hello World!')

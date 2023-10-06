3d3e941b90befecf54384de89a00bff53b7cbb6f
Moiz Ahmed <moizah96@gmail.com>
2023-10-06 16:40:12 -0400

    Added tab-switching logic to switch pages

--- src/index.js
+++ src/index.js
@@ -3,31 +3,47 @@ import { home } from './home.js'
 import { menu } from './menu.js'
 import { contact } from './contact'
 
+const body = document.querySelector("body")
 const content = document.getElementById("content")
 
+// Navigation Bar
 const navBar = document.createElement("div")
 navBar.className = "navBar"
 
 const nav = document.createElement("nav")
 
-
 const homePage = document.createElement("div")
 homePage.className = "home"
 homePage.textContent = "Home"
 nav.appendChild(homePage)
-const contactPage = document.createElement("div")
-contactPage.className = "contact"
-contactPage.textContent = "Contact"
-nav.appendChild(contactPage)
 const menuPage = document.createElement("div")
 menuPage.className = "menu"
 menuPage.textContent = "Menu"
 nav.appendChild(menuPage)
+const contactPage = document.createElement("div")
+contactPage.className = "contact"
+contactPage.textContent = "Contact"
+nav.appendChild(contactPage)
 
 navBar.appendChild(nav)
 
-content.appendChild(navBar)
+body.insertBefore(navBar, body.firstChild)
 
+home()
 
-
-contact();
+// Tab-switching Logic
+nav.addEventListener("click", (e) => {
+    console.log(e.target.className)
+    if (e.target.className == "home") {
+        content.textContent = ""
+        home()
+    }
+    if (e.target.className == "menu") {
+        content.textContent = ""
+        menu()
+    }
+    if (e.target.className == "contact") {
+        content.textContent = ""
+        contact()
+    }
+})

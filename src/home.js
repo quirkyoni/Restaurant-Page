e2d077243b31abab78fe39d603ce5f473385ccc2
Moiz Ahmed <moizah96@gmail.com>
2023-10-08 23:40:14 -0400

    Updated CSS for a simple design to center elements and to work on mobile

--- src/home.js
+++ src/home.js
@@ -7,11 +7,12 @@ const home = () => {
 
     // Header 2
     let hTwo = document.createElement("h2")
+    hTwo.className = "home-header-two"
     hTwo.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores laudantium dolorum voluptate, perspiciatis mollitia esse a sequi officiis laborum harum!"
 
     // Hero Image
-    const heroImage = document.createElement("img")
-    heroImage.src = hero;
+    // const heroImage = document.createElement("img")
+    // heroImage.src = hero;
 
     // Header 4
     let hFour = document.createElement("h4")
@@ -19,7 +20,7 @@ const home = () => {
 
     content.appendChild(hOne)
     content.appendChild(hTwo)
-    content.appendChild(heroImage)
+    // content.appendChild(heroImage)
     content.appendChild(hFour)
 
     console.log('Hello World!')
